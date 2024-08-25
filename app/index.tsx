b905d8a60e6ebb3219ffa0d870008706b16625b8
ImAdityaa12 <adityagupta1291@gmail.com>
2024-08-26 01:06:27 +0530

    Added redirect login

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Image, ScrollView, Text, ToastAndroid, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "@/components/custom-button";
 import { StatusBar } from "expo-status-bar";
 import { Redirect, router } from "expo-router";
+import { useGlobalContext } from "@/context/GlobalProvider";
 const index = () => {
+  const { isLoggedIn } = useGlobalContext();
+  useEffect(() => {
+    if (isLoggedIn) {
+      ToastAndroid.show("User signed in successfully", ToastAndroid.SHORT);
+      router.replace("/home");
+    }
+  }, [isLoggedIn]);
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
